91b499730c534f9244f5934851b1b9938cd744e6
JessicaLorenzon <jelorenzon@gmail.com>
2022-06-21 16:58:16 -0300

    finalizando funcionalidades

---
+++ README.md
@@ -0,0 +1,7 @@
+# aluramidi
+
+Projeto criado durante o curso JavaScript para Web: Crie páginas dinâmicas, da Alura, dentro da [formaçao Front-end](https://cursos.alura.com.br/formacao-front-end).
+
+Você pode ver o resultado final [aqui](https://jessicalorenzon.github.io/aluramidi/).
+
+![aluramidi](aluramidi.png)
--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-document.querySelector('.tecla_pom');
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
+    
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+
+
+
+
+/*
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    //console.log(idAudio);
+    
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+
+    //console.log(contador);
+}
+*/
